4b96d0addf7f22149ea337d4d630987e49d4b193
Cheredko Danil <cheredkodanilo@ukr.net>
2022-05-31 12:24:31 +0300

    init

---
+++ Pr_lab2/Fraction.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace Pr_lab2
+{
+    class Fraction
+    {
+        public double numerator { get; set; }
+        public double denominator { get; set; }
+
+        public Fraction()
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{numerator}/{denominator}");
+        }
+
+        public void Sum(double a, double b)
+        {
+            numerator = numerator * b + a * denominator;
+            denominator = denominator * b;
+            while ((denominator % numerator == 0) && (numerator != 1))
+            {
+                denominator = denominator / numerator;
+                numerator = numerator / numerator;
+            }
+        }
+
+        public double division(double n)
+        {
+            while (n % 1 != 0)
+            {
+                n = n * 10;
+                numerator = numerator * 10;
+            }
+            denominator = denominator * n;
+            while ((denominator % numerator == 0) && (numerator != 1))
+            {
+                denominator = denominator / numerator;
+                numerator = numerator / numerator;
+            }
+            return denominator;
+        }
+
+        public double multiplication(double n)
+        {
+            while (n % 1 != 0)
+            {
+                n = n * 10;
+                denominator = denominator * 10;
+            }
+            numerator = numerator * n;
+            while ((denominator % numerator == 0) && (numerator != 1))
+            {
+                denominator = denominator / numerator;
+                numerator = numerator / numerator;
+            }
+            return numerator;
+        }
+
+        public string Save(Fraction c, string json)
+        {
+            json = JsonConvert.SerializeObject(c);
+            Console.WriteLine(json);
+            return json;
+        }
+
+        public Fraction Input(Fraction c, string json)
+        {
+            c = JsonConvert.DeserializeObject<Fraction>(json);
+            return c;
+        }
+    }
+}
---
+++ Pr_lab2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+
+namespace Pr_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, n;
+            string json = "";
+            Fraction c = new Fraction();
+            c.numerator = int.Parse(Console.ReadLine());
+            c.denominator = int.Parse(Console.ReadLine());
+            c.GetInfo();
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c.Sum(a, b);
+            c.GetInfo();
+            n = double.Parse(Console.ReadLine());
+            c.division(n);
+            c.GetInfo();
+            n = double.Parse(Console.ReadLine());
+            c.multiplication(n);
+            c.GetInfo();
+            c.Save(c, json);
+            c.Input(c, json);
+            c.GetInfo();
+        }
+    }
+}
